88e13125533e4d3653f4f0fd2ec0e26b6f6c537f
AgentHitmanFaris <81744011+AgentHitmanFaris@users.noreply.github.com>
2026-09-17 10:19:10 +0800

    fix(admin): resolve Unexpected identifier 'Proxy' syntax error in admin script

--- _worker.js
+++ _worker.js
@@ -562,12 +562,6 @@ async function 获取前端页面(pathWithQuery, status = 200) {
 			"强力驱动": "Powered by"
 		};
 
-		// Server-side translation
-		for (const [k, v] of Object.entries(dict)) {
-			html = html.replaceAll(k, v);
-		}
-		// Strip emojis
-		html = html.replace(/[\u{1F300}-\u{1F9FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F600}-\u{1F64F}]|[\u{1F680}-\u{1F6FF}]/gu, '');
 
 		const i18nScript = `<script>
 (function() {
